7c05c16079d80fb1749e3145e1c7e4c7c6c00c39
Fernando José dos Santos - FJO <fernando.jose@iteris.local>
2020-02-04 18:00:05 -0300

    first commit

--- app.py
+++ app.py
@@ -1,18 +1,48 @@
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
+from data import students, error
 
 app = Flask(__name__)
 api = Api(app)
 
-class Student_Controller(Resource):
-    def get(self):
-        return {"about":"hello world"}
+@app.route('/students', methods = ['GET'])
+def get():
+    return jsonify(students)
 
-    def post(self):
-        student = request.get_json()
-        return {'you sent': student}, 200
+@app.route('/students/<int:id>', methods = ['GET'])
+def get_by_id(id):
+    for student in students:
+        if student['id']==id:
+            return jsonify(student), 200
+    return jsonify(error), 400
 
-api.add_resource(Student_Controller, '/students')
+@app.route('/students', methods = ['POST'])
+def post():
+    std = request.get_json()
+    for student in students:
+        if student['id']==std['id']:
+            return "Estudante já adicionado.", 400
+    students.append(std)
+    return jsonify(std), 201
+
+@app.route('/students', methods = ['PUT'])
+def put():
+    std = request.get_json()
+    for student in students:
+        if student['id']==std['id']:
+            student['name'] = std['name']
+            student['passport_number'] = std['passport_number']
+            return jsonify(student), 200
+    return jsonify(error), 400
+
+@app.route('/students/<int:id>', methods = ['DELETE'])
+def delete(id):
+    for student in students:
+        if student['id']==id:
+            students.remove(student)
+            return jsonify(student), 200
+    return jsonify(error), 400
+
 
 if __name__ =="__main__":
     app.run(debug=True)
---
+++ data.py
@@ -0,0 +1,13 @@
+students = [
+    {
+        'id': 0,
+        'name': 'Fernando',
+        'passport_number': "12345"
+    }
+]
+
+error = [
+    {
+        'error':'not found'
+    }
+]
